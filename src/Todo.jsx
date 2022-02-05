8c402e43fb9f6e828c66d8d263e099b634d179b4
Coy Sellers <coysellers@gmail.com>
2022-02-05 17:44:19 -0600

    Update list item component

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,43 +4,20 @@ import { v4 as uuidv4 } from 'uuid';
 import './index.css'
 
 function Task({ task }) {
-  const isComplete = task.completed ? 'line-through' : '';
-
   return (
     <li
-      style={{ textDecoration: isComplete }}
+      className="todo-listItem"
+      style={{ textDecoration: task.completed ? "line-through" : "" }}
     >
-      {task.title}
+      <svg className="todo-listImage" strokeLinecap="round" strokeLinejoin="round">
+        <circle cx="12" cy="12" r="11" />
+        <path d={task.completed ? "m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" : "" } fill="none" />
+      </svg>
+      <p className="todo-listText">
+        {task.title}
+      </p>
     </li>
-  )
-}
-
-function CreateTask({ addTask }) {
-  const [value, setValue] = useState('');
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-
-    addTask(value);
-    setValue('');
-  }
-
-  const handleChange = e => {
-    setValue(e.target.value);
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
-        value={value}
-        placeholder='Add a new task'
-        onChange={handleChange}
-      />
-      <button type='submit' value='Submit'>Add Task</button>
-    </form>
-  )
+  );
 }
 
 function Todo() {
@@ -68,39 +45,39 @@ function Todo() {
   }
 
   return (
-    <div class="todo">
+    <div className="todo">
       <img
         src="https://play.tailwindcss.com/img/beams.jpg"
         alt=""
-        class="todo-backgroundImage"
+        className="todo-backgroundImage"
         width="1308"
       />
 
-      <div class="todo-listPosition">
-        <div class="todo-listContainer">
+      <div className="todo-listPosition">
+        <div className="todo-listContainer">
           <h1>Todo List</h1>
-          <ul class="todo-list">
-            <li class="todo-listItem">
-              <svg class="todo-listImage" stroke-linecap="round" stroke-linejoin="round">
-                <circle cx="12" cy="12" r="11" />
-                <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-              </svg>
-              <p class="todo-listText">Customizing your example file</p>
-            </li>
+          <ul className="todo-list">
+            {tasks.map((task, index) => (
+              <Task
+                task={task}
+                index={index}
+                key={index}
+              />
+            ))}
           </ul>
         </div>
 
-        <div class="submit">
-          <form class="submit-form">
+        <div className="submit">
+          <form className="submit-form">
             <input
-              class="submit-formInput"
+              className="submit-formInput"
               type="text"
               aria-label="Add Items to your list"
               placeholder="Add Items to your list..." />
           </form>
-          <div class="submit-buttonContainer">
-            <button class="hover:bg-blue-400 group submit-button">
-              <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
+          <div className="submit-buttonContainer">
+            <button className="hover:bg-blue-400 group submit-button">
+              <svg width="20" height="20" fill="currentColor" className="mr-2" aria-hidden="true">
                 <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
               </svg>
               Add Item
