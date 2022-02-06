2731a74efcce05157463bc0964823ee80112a536
Coy Sellers <coysellers@gmail.com>
2022-02-05 19:53:25 -0600

    Add ability to add and remove list items

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,20 +3,60 @@ import React, { useState, useRef, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import './index.css'
 
-function Task({ task }) {
+function Task({ task, index, removeTask }) {
   return (
-    <li
-      className="todo-listItem"
-      style={{ textDecoration: task.completed ? "line-through" : "" }}
-    >
+    <li className="todo-listItem">
       <svg className="todo-listImage" strokeLinecap="round" strokeLinejoin="round">
         <circle cx="12" cy="12" r="11" />
         <path d={task.completed ? "m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" : "" } fill="none" />
       </svg>
-      <p className="todo-listText">
-        {task.title}
-      </p>
+      <div className="todo-listItemInner">
+        <p className="todo-listText" style={{ textDecoration: task.completed ? "line-through" : "" }}>
+          {task.title}
+        </p>
+        <button className="hover:bg-blue-400 group todo-listItemRemove" onClick={() => removeTask(index)}>
+          Remove
+        </button>
+      </div>
     </li>
+  );
+}
+
+function CreateTask({ addTask }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    addTask(value);
+    setValue('');
+  }
+  return (
+    <div className="submit">
+      <form
+        className="submit-form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="submit-formInput"
+          type="text"
+          value={value}
+          aria-label="Add Items to your list"
+          placeholder="Add Items to your list..."
+          onChange={e => setValue(e.target.value)}
+        />
+
+        
+        <button className="hover:bg-blue-400 group submit-button">
+          <svg width="20" height="20" fill="currentColor" className="mr-2" aria-hidden="true">
+            <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+          </svg>
+          Add Item
+        </button>
+      </form>
+    </div>
   );
 }
 
@@ -44,6 +84,13 @@ function Todo() {
     setTasks(newTasks);
   }
 
+  const removeTask = index => {
+    const newTasks = [...tasks];
+
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
+
   return (
     <div className="todo">
       <img
@@ -62,28 +109,13 @@ function Todo() {
                 task={task}
                 index={index}
                 key={index}
+                removeTask={removeTask}
               />
             ))}
           </ul>
         </div>
 
-        <div className="submit">
-          <form className="submit-form">
-            <input
-              className="submit-formInput"
-              type="text"
-              aria-label="Add Items to your list"
-              placeholder="Add Items to your list..." />
-          </form>
-          <div className="submit-buttonContainer">
-            <button className="hover:bg-blue-400 group submit-button">
-              <svg width="20" height="20" fill="currentColor" className="mr-2" aria-hidden="true">
-                <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-              </svg>
-              Add Item
-            </button>
-          </div>
-        </div>
+        <CreateTask addTask={addTask} />
       </div>
     </div>
   )
